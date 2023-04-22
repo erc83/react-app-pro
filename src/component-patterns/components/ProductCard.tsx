8daab834ea7df6e45c56385ddb310976a3b8ebc4
erc83 <espinoza.eric@gmail.com>
2023-04-22 16:31:09 -0400

    custom hook add

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -1,11 +1,34 @@
+import { useProduct } from '../hooks/useProduct'   // llamar la funcion
 
 import styles from '../style/styles.module.css'
-
-console.log(styles)
+//import noImage from '../assets/no-image.jpg'
 
 export const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
+
+    const { counter, increaseBy } = useProduct()
+
+    return (
+        <div className={ styles.productCard }>
+            <img className={ styles.productImg } src="./coffee-mug.png" alt="" />
+            {/* <img className={ styles.productImg } src={ noImage } alt="" /> */}
+        
+            <span>Coffee Mug</span>
+
+            <div className={ styles.buttonsContainer }>
+                <button 
+                    className={ styles.buttonMinus }
+                    onClick={ () => increaseBy( - 1) } 
+                > - </button>
+                
+                <div className={ styles.countLabel }> { counter } </div>
+
+                <button 
+                    className={ styles.buttonAdd }
+                    onClick={ () => increaseBy( +1 ) }
+                > + </button>
+
+            </div>
+        </div>
   )
 }
 
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -1,13 +1,20 @@
 import { ProductCard } from "../components/ProductCard"
 
-
 export const ShoppingPage = () => {
   return (
     <div>
         ShoppingPage
         <hr />
 
-        <ProductCard />
+        <div 
+          style={{
+            display:"flex",
+            flexDirection: "row",
+            flexWrap: "wrap"
+          }}
+        >
+          <ProductCard />
+        </div>
 
     </div>
   )
