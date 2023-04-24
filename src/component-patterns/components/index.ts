81e9bb9c3f47d3e7cdc98d57b07f1621178362bb
erc83 <espinoza.eric@gmail.com>
2023-04-23 23:51:30 -0400

    assign component to another component

--- src/component-patterns/components/index.ts
+++ src/component-patterns/components/index.ts
@@ -1,8 +1,22 @@
+import { ProductCard as ProductCardHOC } from "./ProductCard";
+import { ProductCardHOCProps } from "../interfaces/interfaces_product";
+
+import { ProductButtons } from "./ProductButton";
+import { ProductImage } from "./ProductImage";
+import { ProductTitle } from "./ProductTitle";
+
+// export { ProductCard } from "./ProductCard";
 export { ProductButtons } from "./ProductButton";
-export { ProductCard } from "./ProductCard";
 export { ProductImage } from "./ProductImage";
 export { ProductTitle } from "./ProductTitle";
 
 
+export const ProductCard: ProductCardHOCProps = Object.assign( ProductCardHOC , {
+    Title: ProductTitle,
+    Image: ProductImage,
+    Buttons: ProductButtons
+})
+
+export default ProductCard; 
 
 
--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -24,11 +24,11 @@ export const ShoppingPage = () => {
             flexWrap: "wrap"
           }}
         >
-          {/* <ProductCard product={ product}>
+          <ProductCard product={ product}>
               <ProductCard.Image />
               <ProductCard.Title />
               <ProductCard.Buttons />    
-          </ProductCard> */}
+          </ProductCard> 
           
           <ProductCard product={ product}>    
               <ProductImage />
